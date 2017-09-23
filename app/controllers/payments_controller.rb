80ce3a37f05860787833d3cc03932534ded75fbb
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2017-09-23 23:20:24 +0900

    create cancel subscribe function

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,10 +3,17 @@ class PaymentsController < ApplicationController
 
   def new
     @payment = Payment.new
+    @payments = Payment.all
   end
 
   def create
     Payment.create_subscription(params['payjp-token'])
     redirect_to new_payment_url, notice: '支払い情報を保存しました'
+  end
+
+  def destroy
+    payment = Payment.find(params[:id])
+    payment.cancel_subscription
+    redirect_to new_payment_url, notice: '定期課金をキャンセルしました'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :payments, only: [:new, :create] do
+  resources :payments, only: [:new, :create, :destroy] do
     post :webhook, on: :collection
   end
 end
